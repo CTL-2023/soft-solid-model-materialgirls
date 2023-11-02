548bdd797f76f73cc8a3a7dc246d734af246d0e1
jpurmann <jpurmann@student.ethz.ch>
2023-11-02 14:33:49 +0100

    bitz wiiter gmacht

--- main.py
+++ main.py
@@ -42,20 +42,35 @@ def create_initial_velocities(N,T):
     return vx, vy
 
 def thermostat(N,T,vx,vy):
-    sumx = 0
-    sumy = 0
-    vxflat = vx.flatten()
+    vxflat = vx.flatten() #create 1D arrays of vx, vy
     vyflat = vy.flatten()
-    ekin1 = N * T
-    ekin2 = np.dot(vx.flatten(),vx.flatten()) + np.dot(vy.flatten(),vy.flatten())
-    for i in vx:
-        sumx += np.sum(i**2)
-    for j in vy:
-        sumy += np.sum(j**2)
+    ekin1 = (N**2) * T
+    ekin2 = np.dot(vxflat,vxflat) + np.dot(vyflat,vyflat) #dot product = squared velocities
+    placeholder = ekin1/ekin2 #calculate factor so ekin1 and ekin2 are equal
+    ekinnew = ekin2 * placeholder 
+    vxnew = vx * np.sqrt(placeholder) #times placeholder, so that ekin1 =ekin2 are the same
+    vynew = vy * np.sqrt(placeholder)
+    vxnew -= np.mean(vxnew)
+    vynew -= np.mean(vynew)
+        
+    return vxnew, vynew
 
-    ekin3 = np.sum(sumx, sumy)
-    return vxflat, vyflat
-print(thermostat(N,T,create_initial_velocities))
+def fold(L,x): #not sure if right like this
+    x -= L*round(x/L)
+    return x
+
+def get_dist(L,x1,y1,x2,y2):
+    distance_vector = fold(L,[x1,y1] - [x2,y2])
+    distance = norm(distance_vector)
+    return distance_vector,distance
+
+def get_forces(N,L,X,Y):
+
+    
+    return
+
+
+#print(thermostat(N,T,*create_initial_velocities(N,T)))
 #print(create_initial_velocities(N,T))
 #grid_nodes = create_initial_configuration(g, N)
 
